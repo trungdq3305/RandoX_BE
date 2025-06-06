034ea57667efb96d60699873b5d684cb848b342a
trungdq3305 <fccdotm1@gmail.com>
2025-06-06 16:50:38 +0700

    .

--- src/RandoX.API/Program.cs
+++ src/RandoX.API/Program.cs
@@ -100,12 +100,10 @@ builder.Services.AddLogging();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+
 
 app.UseHttpsRedirection();
 
